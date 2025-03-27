afa227f6cda80629e61714098bcf32fa94aeb33b
Siddhant Mohan <44978626+siddhantmohan1110@users.noreply.github.com>
2025-03-27 01:43:20 -0400

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -11,8 +11,8 @@ fi
 # Run vosk-transcriber (ensure the audio and model files exist)
 # Replace the following with your actual input file paths and model names
 # Example: assume you have mounted or copied the model and audio into /app
-if [ -f "audio/youtube.mp4" ]; then
+if [ -f "demovideo.MP4" ]; then
     vosk-transcriber -n vosk-model-en-us-0.42-gigaspeech -i demovideo.MP4 -o test.txt
 else
-    echo "audio/youtube.mp4 not found. Skipping transcription."
+    echo "demovideo.MP4 not found. Skipping transcription."
 fi
